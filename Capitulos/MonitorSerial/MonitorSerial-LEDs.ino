6bb52082803989a6a9cb00e823d84a53e43ebb02
LeonardoVNC <165107546+LeonardoVNC@users.noreply.github.com>
2024-07-26 14:30:25 -0400

    Se agrega un programa de apoyo para el Monitor Serial

---
+++ Capitulos/MonitorSerial/MonitorSerial-LEDs.ino
@@ -0,0 +1,36 @@
+String input = "";
+char c;
+int led = 8;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  Serial.println("\nInicio del Programa");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    c = Serial.read();
+    if (c == '\n' || c == '\r') {
+      input.toLowerCase();
+      input.replace(" ","");
+      if (input.equals("encender")) {
+        digitalWrite(led, HIGH);
+      } else if (input.equals("apagar")) {
+        digitalWrite(led, LOW);
+      } else {
+        int intput = input.toInt();
+        if (intput != 0) {
+          for (int i = 0; i < intput; i++) {
+            digitalWrite(led, HIGH);
+            delay(500);
+            digitalWrite(led, LOW);
+            delay(500);
+          }
+        }
+      }
+      input = "";
+    } else {
+      input += c;
+    }
+  }
+}
